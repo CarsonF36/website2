91c651fc5a807f32d8930b51ae0d35e94c72848e
Carson Feeser <142545570+CarsonFeeser@users.noreply.github.com>
2024-03-19 17:18:38 +0000

    Tue, Mar 19, 2024, 10:18 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,6 +1,20 @@
 rules = document.getElementById('rules-btn')
 hiderules = document.getElementById('close-btn')
 toggle = document.querySelector('.rules')
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
+
+// Creare Ball Properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height /2,
+    size: 10,
+    speed: 4,
+    dx: 4;
+    dy: -4;
+}
+
+//Draw Ball on Canvas
 
 
 // Rules open and close event handlers
