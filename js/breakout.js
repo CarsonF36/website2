0e1555004c8d47f9e04dca7cbeede64eac6680c6
Carson Feeser <142545570+CarsonFeeser@users.noreply.github.com>
2024-03-20 17:01:54 +0000

    Wed, Mar 20, 2024, 10:01 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,65 +1,9 @@
 rules = document.getElementById('rules-btn')
 hiderules = document.getElementById('close-btn')
 toggle = document.querySelector('.rules')
-canvas = document.getElementById('canvas')
-ctx = canvas.getContext('2d')
 score = 0
-
-// Creare Ball Properties
-ball = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
-    size: 10,
-    speed: 4,
-    dx: 4,
-    dy: -4,
-}
-//Create Paddle properties
-paddle = {
-    x: canvas.width / 2,
-    y: canvas.height - 20,
-    w: 80,
-    h: 10,
-    speed: 8,
-    dx: 0,
-}
-
-
-
-
-//Draw Ball on Canvas
-function drawBall() {
-    ctx.beginPath()
-    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
-    ctx.fillStyle = '#009599'
-    ctx.fill()
-    ctx.closePath()
-}
-
-
-//Draw Paddle on Canvas
-function drawPaddle() {
-    ctx.beginPath()
-    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
-    ctx.fillStyle = '#009599'
-    ctx.fill()
-    ctx.closePath()
-}
-
-//Draw score on canvas
-function drawScore() {
-    ctx.font = '20px Arial'
-    ctx.fillText('Score: ${score}, canvas.width-100, 30')
-}
-
-
-//Draw Everything
-function draw() {
-    drawPaddle()
-    drawBall()
-}
-
-draw()
+brickRowCount = 9
+brickColumnCount = 5
 
 // Rules open and close event handlers
 rules.addEventListener('click', () => {
@@ -69,3 +13,76 @@ rules.addEventListener('click', () => {
 hiderules.addEventListener('click', () => {
     toggle.classList.remove('show')
 })
+
+
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+
+// Create ball properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+}
+
+
+// Draw ball on canvas
+function drawBall() {
+    ctx.beginPath()
+    ctx.arc(ball.x,ball.y,ball.size,0,Math.PI * 2)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
+}
+
+
+// Create paddle properties
+paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
+}
+
+//Create Brick properties
+brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+}
+
+
+// Draw paddle on canvas
+function drawPaddle() {
+    ctx.beginPath()
+    ctx.rect(paddle.x,paddle.y,paddle.w,paddle.h)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
+}
+
+
+// Draw score on canvas
+function drawScore() {
+    ctx.font = '20px Arial'
+    ctx.fillText(`Score: ${score}`, canvas.width-100, 30)
+}
+
+
+// Draw everything
+function draw() {
+    drawPaddle()
+    drawBall()
+    drawScore()
+}
+
+draw()
