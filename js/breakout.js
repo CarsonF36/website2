32c279f136ee553106366367c3322060c4901d2b
Carson Feeser <142545570+CarsonFeeser@users.noreply.github.com>
2024-04-02 16:52:04 +0000

    Tue, Apr 2, 2024, 9:52 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -187,9 +187,10 @@ if (
                 ball.x - ball.size > brick.x && //left brick side
                 ball.x + ball.size < brick.x + brick.w && //right
                 ball.y - ball.size < brick.y + brick.h //bottom
-            )
+            ) {
             ball.dy = -1 * ball.dy
             brick.visible = false
+            }
         }
     })
  })
