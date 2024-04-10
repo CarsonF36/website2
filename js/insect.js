a9a306f5201fc8aeeeb654930dacf6deef6d9019
Carson Feeser <142545570+CarsonFeeser@users.noreply.github.com>
2024-04-10 17:20:41 +0000

    Wed, Apr 10, 2024, 10:20 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -77,7 +77,9 @@ function increaseScore() {
     scoreEl.innerHTML = `Score: ${score}`
 }
 
-if (score < 60)
+if (score < 60 || seconds < 30) {
+    message2.classList.add('visible')
+}
 
 function getRandomLocation() {
     const width = window.innerWidth
