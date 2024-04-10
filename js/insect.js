66a1a36ef1ebf1805024c6af2b9ae88c783e088b
Carson Feeser <142545570+CarsonFeeser@users.noreply.github.com>
2024-04-10 15:52:45 +0000

    Wed, Apr 10, 2024, 8:52 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,2 +1,7 @@
 const screens = document.querySelectorAll('.screen')
-
+const choose_insect_btns = querySelectorAll('.choose-insect-btn')
+const game_container = document.getElementById('game-container')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getElememtById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
