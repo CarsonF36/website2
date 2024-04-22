af02dae92a74c8568e3364779d586af54e3e1ef7
Carson Feeser <142545570+CarsonFeeser@users.noreply.github.com>
2024-04-22 16:50:10 +0000

    Mon, Apr 22, 2024, 9:50 AM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -9,3 +9,4 @@ const figureParts = document.querySelectorAll('.figure-part')
 const word = ['application', 'programming', 'interface', 'wizard']
 
 let selectedIndex = Math.floor(word.length * Math.random())
+let selectedWord = word[selectedIndex]
