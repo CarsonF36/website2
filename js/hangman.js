d3c6bb0325e4c40624cf70000a402a6098c63e7e
Carson Feeser <142545570+CarsonFeeser@users.noreply.github.com>
2024-04-23 17:12:20 +0000

    Tue, Apr 23, 2024, 10:12 AM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -1,8 +1,8 @@
 const wordEl = document.getElementById('word')
 const wrongLettersEl = document.getElementById('wrong-letters')
-const playAgainBtn = document.getElementById('play-again')
+const playAgainBtn = document.getElementById('play-button')
 const popup = document.getElementById('popup-container')
-const notification = document.getElementById('notification-center')
+const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
 const figureParts = document.querySelectorAll('.figure-part')
 
@@ -14,36 +14,92 @@ let selectedWord = word[selectedIndex]
 const correctLetters = []
 const wrongLetters = []
 
-// Show Hidden Word
 function displayWord() {
     wordEl.innerHTML = `
-        ${selectedWord
-        .split('')
-        .map(letter => `
-        <span class="letters">
+    ${selectedWord.split('').map(letter => `
+        <span class="letter">
             ${correctLetters.includes(letter) ? letter : ''}
-        <span>
-        `).join('')
-        }
-    `
+        </span>
+    `).join('')}
+`
     const innerWord = wordEl.innerText.replace(/\n/g, '')
-
-    if (innerWord == selectedWord) {
+    if(innerWord == selectedWord){
         finalMessage.innerText = 'Congratulations! You won!'
+        popup.stly.display = 'flex'
+    }
+}
+
+
+//update wrong letters
+function updateWrongLettersEl(){
+    wrongLettersEl.innerHYML= `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `
+    figureParts.forEach((part, index) => {
+        const erros = wrongLetters.length
+
+        if(index < errors){
+            part.style.display ='block'
+        } else{
+            part.style.display = 'none'
+        }
+    })
+
+
+    if(wrongLetters.length == figureParts.length){
+        finalMessage.innerText = 'Unfortunately you lost!'
         popup.style.display = 'flex'
     }
 }
 
-// Update the wrong letters
-function updateWrongLetters() {
-    wrongLettersEl.innerHTML = `
-        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
-        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
 
-    `
+//show notification
+function showNotification(){
+    notification.classList.add('show')
+
+    setTimeout(() => {
+        notifciation.classList.remove('show')
+    }, 2000)
 }
 
-// Show Notification
-function showNotification() {
 
+//keydown letter press
+window.addEventListener('keydown', e =>{
+if(e.keyCode >= 65 && e.keyCode <=90){
+    const letter = e.key
+
+    if(selectedWord.includes(letter)){
+        if(!correctLetters.includes(letter)){
+            correctLetters.push(letter)
+            displayWord()
+        } else{
+            showNotification()
+        }
+    } else{
+        if(!wrongLetters.include(letter)){
+            wrongLetters.push(letter)
+
+            updateWrongLettersEl()
+        } else {
+            showNotification()
+        }
+    }
 }
+})
+
+playAgainBtn.addEventListener('click', () => {
+correctLetters.length = 0
+wrongLetters.length = 0
+selectedIndex = Math.floor(word.length * Math.random())
+selectedWord = word[selectedIndex]
+displayWord()
+
+updateWrongLettersEl()
+
+popup.style.display= 'none'
+
+})
+
+
+displayWord()
