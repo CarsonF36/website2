8dd17eab902e1367d7a2b4ef1524ce1f386ddf74
Carson Feeser <142545570+CarsonF36@users.noreply.github.com>
2024-05-20 14:13:50 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     toggleButton.addEventListener('click', function () {
         isColored = !isColored;
         if (isColored) {
-            body.style.backgroundColor = 'yellow';
+            body.style.backgroundColor = 'chartreuse';
         } else {
             body.style.backgroundColor = 'skyblue';
         }
